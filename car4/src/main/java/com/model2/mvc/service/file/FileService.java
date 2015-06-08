c1876c9ad127967b70002631b4e77c7b8467af3c
KimYoungBum <gre0507@gmail.com>
2015-06-08 20:18:30 +0900

    .

--- car4/src/main/java/com/model2/mvc/service/file/FileService.java
+++ car4/src/main/java/com/model2/mvc/service/file/FileService.java
@@ -1,11 +1,16 @@
 package com.model2.mvc.service.file;
 
+import java.util.List;
+
 import com.model2.mvc.common.FileUpload;
 import com.model2.mvc.service.domain.Car;
 
 public interface FileService {
 		
 	public void addFile(FileUpload file) throws Exception;
+
 	public FileUpload getFile(int carNo) throws Exception;
 
+	public List<FileUpload> getFileList(int carNo) throws Exception;
+	
 }
